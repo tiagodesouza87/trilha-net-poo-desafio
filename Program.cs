bcbdfb7350dc1284f584b6cecaea70eea47e1093
Tiago de Souza <tiago@pop-os.localdomain>
2022-09-16 11:33:48 -0300

    Projeto realizado.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,7 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Iphone MeuIphone = new Iphone("83 99566-2233", "20S","123ADC",128);
+Nokia MeuNokia = new Nokia("83 9888-7788", "X23","456OLD",128);
+
+MeuIphone.InstalarAplicativo("Angry Birds");
+MeuNokia.InstalarAplicativo("Netflix");
